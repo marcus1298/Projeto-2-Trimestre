cdc3eeaacd0c872d19f815c8e97fda8f752c8385
Marcus 1298 <marcusviniciusiftm@gmail.com>
2018-06-18 11:26:43 -0300

    Movimentação basica na horizonal

--- Assets/Scripts/Player Ctrl.cs
+++ Assets/Scripts/Player Ctrl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainMenuUICtrl : MonoBehaviour {
+public class PlayerCtrl : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+public float horizontalSpeed = 10f;
+	// Use this for initialization
+
+
+Rigidbody2D rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontalInput = Input.GetAxisRaw("Horizontal");//-1; esquerda, 1:direita
+		float horizontalPlayerSpeed = horizontalSpeed * horizontalInput;
+		if(horizontalPlayerSpeed !=0){
+			MoveHorizontal(horizontalSpeed);
+		}
+		else{
+			StopMoving();
+		}
+	}
+	void MoveHorizontal(float speed){
+		rb.velocity = new Vector2(speed, rb.velocity.y);
+
+	}
+	void StopMoving(){
+		rb.velocity = new Vector2(0f, rb.velocity.y);
+	}
+}
