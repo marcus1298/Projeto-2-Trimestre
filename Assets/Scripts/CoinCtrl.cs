08612f508631bf7b6c2c3bda2eea6e2206c13e94
marcus1298 <marcusviniciusiftm@gmail.com>
2018-08-10 08:07:05 -0300

    Pegando Moedas

--- Assets/Scripts/CoinCtrl.cs
+++ Assets/Scripts/CoinCtrl.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 public class CoinCtrl : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.gameObject.CompareTag("Player")){
+			Destroy(gameObject);
+		}
 	}
 }
