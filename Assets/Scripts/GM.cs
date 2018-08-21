3a96479e21879e4bf5ee56344de2a5ddce012b9b
marcus1298 <marcusviniciusiftm@gmail.com>
2018-08-21 09:04:42 -0300

    Timer

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -7,11 +7,15 @@ public class GM : MonoBehaviour {
 	// Use this for initialization
 	public static GM instance = null;
 	public float yMinLive = -10f;
+	public PlayerController player;
 	public Transform spawnPoint;
 	public GameObject playerPrefab;
-	public PlayerController player;
-
 	public float timeToRespaw = 2f;
+
+	
+	public float maxTime = 120f;
+	bool TimerOn = true;
+	float timeLeft;
 	
 	public UI ui;
 	GameData data = new GameData();
@@ -26,6 +30,7 @@ public class GM : MonoBehaviour {
 		if(player==null){
 			RespawnPlayer();
 		}
+		timeLeft = maxTime;
 	}
 	
 	// Update is called once per frame
@@ -36,21 +41,31 @@ public class GM : MonoBehaviour {
 			player = obj.GetComponent<PlayerController>();
 		}		
 		}
+	UpdateTimer();
 	 DisplayHudData();
+	}
+	void UpdateTimer(){
+		if(TimerOn){
+			timeLeft = timeLeft - Time.deltaTime;
+			if(timeLeft <=0){
+				timeLeft = 0;
+				ExpirePlayer();
+			}
+		}
+
 	}
 	
 
 	void DisplayHudData(){
 	ui.hud.txtCoinCount.text = "x " + data.coinCount;
+	ui.hud.txtTimer.text = "Timer: " + timeLeft.ToString("F1");
 	}
 	public void IncrementCoinCount()
 	{
 		data.coinCount++;
 	}
 
-	public void RespawnPlayer(){
-		Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
-	}
+
 	
 	public void KillPlayer(){
 		if(player!=null){
@@ -58,5 +73,20 @@ public class GM : MonoBehaviour {
 			Invoke("RespawnPlayer", timeToRespaw);
 		}
 	}
-	
+	public void ExpirePlayer(){
+		if(player!=null){
+			Destroy(player.gameObject);
+		}
+		GameOver();
+	}
+		void GameOver(){
+			TimerOn = false;
+			ui.gameOver.txtCoinCount.text = "Coins: " + data.coinCount;
+			ui.gameOver.txtTimer.text = "Timer: " +  timeLeft.ToString("F1");
+			ui.gameOver.gameOverPanel.SetActive(true);
+		}
+
+		public void RespawnPlayer(){
+		Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+	}
 }
