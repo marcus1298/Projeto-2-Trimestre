0be86a31f90e51fdc156fd74e326d1f638c83c4a
CavanhaMan <cavanhaman@hotmail.com>
2018-08-24 08:11:16 -0300

    Contando Vidas

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -58,19 +58,24 @@ public class GM : MonoBehaviour {
 
 	void DisplayHudData(){
 	ui.hud.txtCoinCount.text = "x " + data.coinCount;
+	ui.hud.txtLifeCount.text = "x " + data.lifeCount;
 	ui.hud.txtTimer.text = "Timer: " + timeLeft.ToString("F1");
 	}
 	public void IncrementCoinCount()
 	{
 		data.coinCount++;
 	}
-
+	public void DecrementLives(){
+		data.lifeCount--;
+	}
 
 	
 	public void KillPlayer(){
 		if(player!=null){
 			Destroy(player.gameObject);
-			Invoke("RespawnPlayer", timeToRespaw);
+			DecrementLives();
+			if(data.lifeCount>0){
+				Invoke("RespawnPlayer", timeToRespaw);}
 		}
 	}
 	public void ExpirePlayer(){
